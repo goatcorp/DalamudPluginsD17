e3034b6059b878358bcae5e415570d3be47e0ef1
Oowazu Nonowazu <83298827+nonowazu@users.noreply.github.com>
2022-08-01 12:18:42 -0500

    Add version checking to manifest validation (#25)
    add version validation

--- pyckager/models.py
+++ pyckager/models.py
@@ -5,6 +5,7 @@ from pydantic import BaseModel, validator
 
 
 NUMBERS_ONLY = compile(r'^\d+$')
+VERSION = compile(r'^(\d+\.)+\d+$')
 
 class Plugin(BaseModel):
     repository: str
@@ -12,6 +13,7 @@ class Plugin(BaseModel):
     owners: list[str]
     project_path: Optional[str]
     changelog: Optional[str]
+    version: Optional[str]
 
     @validator('owners')
     def validate_owners(cls, owners):
@@ -19,6 +21,16 @@ class Plugin(BaseModel):
             if NUMBERS_ONLY.match(owner):
                 raise ValueError(f'Value "{owner}" must be a github username; ids are not allowed')
         return owners
+
+    @validator('version')
+    def validate_version(cls, version):
+        version_match = VERSION.match(version)
+        if version_match:
+            segments = tuple(int(x) for x in version_match[0].split('.'))
+            if 1 < len(segments) < 5:
+                return segments
+        raise ValueError(f'Value "{version}" is not a valid version')
+
 
 class Manifest(BaseModel):
     plugin: Plugin
